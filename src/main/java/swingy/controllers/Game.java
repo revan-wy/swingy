ab814f64e5961c7b93efc430146abc4c3494b864
Renzo VAN WYK <revan-wy@c2r5s4.wethinkcode.co.za>
2019-07-24 18:19:10 +0200

    WIP

--- src/main/java/swingy/controllers/Game.java
+++ src/main/java/swingy/controllers/Game.java
@@ -1,9 +1,13 @@
 package swingy.controllers;
 
+import swingy.models.GetInput;
 import swingy.views.*;
 
 public class Game {
     public static void main (String[] args) {
+        String input;
         Show.initialScreen();
+        input = GetInput.read();
+        // add here what to do with input and how to proceed
     }
 }
---
+++ src/main/java/swingy/models/GetInput.java
@@ -0,0 +1,30 @@
+package swingy.models;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class GetInput {
+    static BufferedReader br;
+
+    public static void makeBr() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+
+    public static String read() {
+        String input;
+        if (br == null) {
+            makeBr();
+        }
+        try {
+            input = br.readLine();
+            return input;
+        } catch (IOException e) {
+            System.out.println("Reading input failed");
+            System.exit(1);
+            return "";
+        }
+
+    }
+}
