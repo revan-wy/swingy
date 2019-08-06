49f58157791a9ad2b6e733a43d32eed1e4cf2df9
Renzo VAN WYK <revan-wy@c2r6s5.wethinkcode.co.za>
2019-08-07 00:17:11 +0200

    updated Fighter constructor

--- src/main/java/swingy/models/Fighter.java
+++ src/main/java/swingy/models/Fighter.java
@@ -14,36 +14,41 @@ public class Fighter {
         this.fighterType = setFighterClass(fighterClass);
         this.level = level;
         switch(this.fighterType) {
-            case BLIGHT:
-                this.attack = 160;
-                this.defence = 160;
-                this.maxHitPoints = 160;
-                break;
+            // case BLIGHT:
+            //     this.attack = 160;
+            //     this.defence = 160;
+            //     this.maxHitPoints = 160;
+            //     break;
             case SOLDIER:
                 this.attack = 80;
                 this.defence = 80;
                 this.maxHitPoints = 160;
                 break;
-            case OOZE:
-                this.attack = 160;
-                this.defence = 160;
-                this.maxHitPoints = 160;
-                break;
+            // case OOZE:
+            //     this.attack = 160;
+            //     this.defence = 160;
+            //     this.maxHitPoints = 160;
+            //     break;
             case ROGUE:
                 this.attack = 160;
                 this.defence = 80;
                 this.maxHitPoints = 80;
                 break;
-            case SKELETON:
-                this.attack = 160;
-                this.defence = 160;
-                this.maxHitPoints = 160;
-                break;
+            // case SKELETON:
+            //     this.attack = 160;
+            //     this.defence = 160;
+            //     this.maxHitPoints = 160;
+            //     break;
             case THIEF:
                 this.attack = 80;
                 this.defence = 160;
                 this.maxHitPoints = 80;
                 break;
+            default:
+            this.attack = 80;
+            this.defence = 160;
+            this.maxHitPoints = 80;
+            break;
         }
         this.remainingHitPoints = this.maxHitPoints;
     }
